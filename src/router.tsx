d8f5b6bc55b31e9b06d5fad53df189fcd0dce3fa
Jairwin <jairwin@jairwin.cn>
2022-05-21 14:46:34 +0800

    refactor: 优化路由书写方式

--- src/router.tsx
+++ src/router.tsx
@@ -1,40 +1,97 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import Main from './pages/main'
-import Category from './pages/category'
-import Shop from './pages/shop'
-import Detail from './pages/shop/detail'
-import Mine from './pages/mine'
-import Collection from './pages/mine/collection'
-import Help from './pages/mine/help'
-import Order from './pages/mine/order'
-import ChangePassword from './pages/auth/change-password'
-import Ad from './pages/mine/ad';
-import Address from './pages/mine/address/list'
-import AddressAdd from './pages/mine/address/add'
-import AddressEdit from './pages/mine/address/edit'
-import Login from './pages/auth/login'
-import Demo from './pages/demo/demo'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Main from '@pages/main'
+import Category from '@pages/category'
+import Shop from '@pages/shop'
+import Detail from '@pages/shop/detail'
+import Mine from '@pages/mine'
+import Collection from '@pages/mine/collection'
+import Help from '@pages/mine/help'
+import Order from '@pages/mine/order'
+import ChangePassword from '@pages/auth/change-password'
+import Ad from '@pages/mine/ad';
+import Address from '@pages/mine/address/list'
+import AddressAdd from '@pages/mine/address/add'
+import AddressEdit from '@pages/mine/address/edit'
+import Login from '@pages/auth/login'
 import NotFound from '@pages/auth/not-found'
+import { ReactNode } from 'react'
+
+interface IRouter {
+	path: string;
+	element: ReactNode;
+}
+
+const routers: IRouter[] = [
+	{
+		path: '/',
+		element: <Main />,
+	},
+	{
+		path: '/category',
+		element: <Category />,
+	},
+	{
+		path: '/shop',
+		element: <Shop />,
+	},
+	{
+		path: '/detail/:id',
+		element: <Detail />,
+	},
+	{
+		path: '/mine',
+		element: <Mine />,
+	},
+	{
+		path: '/order',
+		element: <Order />,
+	},
+	{
+		path: '/help',
+		element: <Help />,
+	},
+	{
+		path: '/collection',
+		element: <Collection />,
+	},
+	{
+		path: '/change-password',
+		element: <ChangePassword />,
+	},
+	{
+		path: '/ad',
+		element: <Ad />,
+	},
+	{
+		path: '/address/list',
+		element: <Address />,
+	},
+	{
+		path: '/address/add',
+		element: <AddressAdd />,
+	},
+	{
+		path: '/address/edit',
+		element: <AddressEdit />,
+	},
+	{
+		path: '/auth/login',
+		element: <Login />,
+	},
+	{
+		path: '*',
+		element: <NotFound />,
+	},
+]
 
 const Router = () => (
 	<BrowserRouter>
 		<Routes>
-			<Route path="/" element={<Main />} />
-			<Route path="/category" element={<Category />} />
-			<Route path="/shop" element={<Shop />} />
-			<Route path="/detail/:id" element={<Detail />} />
-			<Route path="/mine" element={<Mine />} />
-			<Route path="/order" element={<Order />} />
-			<Route path="/help" element={<Help />} />
-			<Route path="/collection" element={<Collection />} />
-			<Route path="/change-password" element={<ChangePassword />} />
-			<Route path="/ad" element={<Ad />} />
-			<Route path="/address/list" element={<Address />} />
-			<Route path="/address/add" element={<AddressAdd />} />
-			<Route path="/address/edit" element={<AddressEdit />} />
-			<Route path="/auth/login" element={<Login />} />
-			<Route path="/demo" element={<Demo />} />
-			<Route path="*" element={<NotFound />} />
+			{
+				routers.map((item: IRouter) => {
+					return <Route key={item.path} path={item.path} element={item.element} />
+				})
+			}
 		</Routes>
 	</BrowserRouter>
 )
