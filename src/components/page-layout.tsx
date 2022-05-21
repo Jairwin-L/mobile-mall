f77b09588fe62e6644417f9f33f577cd0dbd6c56
Jairwin <jairwin@jairwin.cn>
2022-05-21 14:59:44 +0800

    增加文件

---
+++ src/components/page-layout.tsx
@@ -0,0 +1,19 @@
+import { Button, ErrorBlock } from "antd-mobile";
+import { Footer, MobileTabBar } from "@components";
+import Loading from "./loading";
+
+export default ({ success = true, loading = false, footerFlag = false, tabbarFlag = false, children, onRefresh }: any) => {
+  return (
+    <>
+      {!loading && success && <>{children}</>}
+      {!success && (
+				<ErrorBlock fullPage>
+					<Button size='small' color='primary' onClick={() => onRefresh && onRefresh()}>刷新</Button>
+				</ErrorBlock>
+			)}
+      {loading && success && <Loading />}
+			{footerFlag && <Footer />}
+			{tabbarFlag && <MobileTabBar />}
+    </>
+  );
+};
